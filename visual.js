dc57764845c7a79cf9affc075bb99042b2d04cc0
Yann Serinet <yann.serinet@hagergroup.com>
2020-04-02 08:07:11 +0200

    visual (input, select, checkbox)

--- visual.js
+++ visual.js
@@ -4,10 +4,31 @@ const { PAGES } = require('../../configuration/index');
 import { Given, Then } from 'cypress-cucumber-preprocessor/steps';
 
 
-
 Then('I can see {string} label is {string}', (labelName, value) => {
     var classPage = Cypress.env('classPage')
     classPage.checkLabel(labelName, value)
+})
+
+
+Then('I can see {string} input is {string}', (inputName, value) => {
+    var classPage = Cypress.env('classPage')
+    classPage.checkInput(inputName, value)
+})
+
+Then('I can see {string} select value is {string}', (selectName, value) => {
+    var classPage = Cypress.env('classPage')
+    classPage.checkSelect(selectName, value)
+})
+
+Then('I can see {string} checkbox is {string}', (checkboxName, value) => {
+
+    const CHECKBOX_STATUSES = {
+        'checked': 'be.checked',
+        'unchecked' : 'not.be.checked'
+    }
+
+    var classPage = Cypress.env('classPage')
+    classPage.checkCheckbox(checkboxName, CHECKBOX_STATUSES[value])
 })
 
 Then('I see {string} in the title', (title) => {
